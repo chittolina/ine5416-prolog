21b2efd54a018df38916b0f0ec89f4ba7c0a8b4d
Gabriel Chittolina <gabrielchittolina1@gmail.com>
2017-03-30 08:54:59 -0300

    Fix program indendation

--- t1a/programa.pl
+++ t1a/programa.pl
@@ -1,22 +1,36 @@
 :- consult('cco.pl').
 
 % Questão 1
-discfase(D,F) :- disciplina(F, D, _).
+discfase(D,F) :-
+	disciplina(F, D, _).
 
 % Questão 2
-prereqfase(Dpr,F) :- prerequisito(Dpr,_) , discfase(Dpr,F).
+prereqfase(Dpr,F) :-
+	prerequisito(Dpr,_) ,
+	discfase(Dpr,F).
 
 % Questão 3
-nomeprereqnome(Npr,N) :- disciplina(_,Dpr,Npr) , prerequisito(Dpr,D) , disciplina(_,D,N).
+nomeprereqnome(Npr,N) :-
+	disciplina(_,Dpr,Npr) ,
+	prerequisito(Dpr,D) ,
+	disciplina(_,D,N).
 
 % Questão 4
-nomeprereqfase(Npr,Dpr,Fpr) :-  prerequisito(Dpr,_) , disciplina(Fpr,Dpr,Npr).
+nomeprereqfase(Npr,Dpr,Fpr) :-
+	prerequisito(Dpr,_) ,
+	disciplina(Fpr,Dpr,Npr).
 
 % Questão 5
-prereqdiscfase(Dpr,D,F) :- prerequisito(Dpr,D), discfase(D,F).
+prereqdiscfase(Dpr,D,F) :-
+	prerequisito(Dpr,D) ,
+	discfase(D,F).
 
 % Questão 6
-prereqdiscfaseprereq(Dpr,D,Fpr) :- discfase(Dpr,Fpr) , prereqdiscfase(Dpr,D,_).
+prereqdiscfaseprereq(Dpr,D,Fpr) :-
+	discfase(Dpr,Fpr) ,
+	prereqdiscfase(Dpr,D,_).
 
 % Questão 7
-nomeprereqdiscfase(Npr,Dpr,D,F) :- disciplina(_,Dpr,Npr) , prereqdiscfase(Dpr,D,F).
+nomeprereqdiscfase(Npr,Dpr,D,F) :-
+	disciplina(_,Dpr,Npr) ,
+	prereqdiscfase(Dpr,D,F).
